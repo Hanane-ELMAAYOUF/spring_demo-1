4d456fe6c82911a9b62afc644512561c761cbe28
Hanane ELMAAYOUF <hananeelmaayouf17@gmail.com>
2022-01-17 22:05:42 +0100

    update-aspect-fonctionnel-03

--- src/country/dao/CountryDAOImp.java
+++ src/country/dao/CountryDAOImp.java
@@ -1,30 +1,16 @@
 package country.dao;
 
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
-import javax.sql.DataSource;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import country.model.Continent;
 import country.model.Country;
 @Repository("hibernate")
 public class CountryDAOImp implements CountryDAO{
 	@Autowired
     SessionFactory  sessionFactory;
-	@Autowired
-	DataSource dataSource;
 	
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
@@ -32,8 +18,8 @@ public class CountryDAOImp implements CountryDAO{
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
+	
 	@Override
-
 	public int add(Country country,String nameOfContinet) {
 		country.setContinent(getByName(nameOfContinet));
         if(country.getContinent()==null) {
@@ -50,17 +36,14 @@ public class CountryDAOImp implements CountryDAO{
 	}
 	@Override
 	public Continent getByName(String name) {
-		
-		String hql="from Continent C where C.name =:name";
-		Query query=getSessionFactory().openSession().createQuery(hql);
+		Query query=getSession().createQuery("from Continent C where C.name =:name");
 		query.setParameter("name", name);
 		return (Continent) query.uniqueResult();
 	}
 
 @Override
 	public Country getByCode(String code) {
-		String hql="from Country C where C.code =:code";
-		Query query=getSessionFactory().openSession().createQuery(hql);
+		Query query=getSession().createQuery("from Country C where C.code =:code");
 		query.setParameter("code", code);
 		Country country=(Country) query.uniqueResult();
         return country;
--- src/country/service/impl/ServiceWorkerImpl.java
+++ src/country/service/impl/ServiceWorkerImpl.java
@@ -2,11 +2,9 @@ package country.service.impl;
 
 import country.dao.CountryDAO;
 import country.model.Country;
-import country.service.ICountryService;
 import country.service.IServiceWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +12,6 @@ public class ServiceWorkerImpl implements IServiceWorker {
 	@Autowired
 	@Qualifier("hibernate")
 	private CountryDAO countryDAO;
-	@Autowired
-	private ApplicationContext applicationContext;
 	
 	@Override
 	public void InsertCountry(Country country,String nameOfContinet) {
